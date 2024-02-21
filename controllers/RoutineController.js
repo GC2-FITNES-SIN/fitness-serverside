8545be08630153b5964ee4fabc6e31337efae859
Rahmah Putri Azzahra <rahmaputriazzahra@gmail.com>
2024-02-21 11:29:34 +0700

    fix: merge issue

--- controllers/RoutineController.js
+++ controllers/RoutineController.js
@@ -3,63 +3,58 @@ const Routines = require("../models/routines"); // Adjust the path as necessary 
 const { redis } = require("../config/redisConn.js");
 
 class RoutineController {
-  
-    // Get all routines or filter by category if a category query parameter is provided
-    static async getAllRoutines(req, res, next) {
-        const { search } = req.query;
+  // Get all routines or filter by category if a category query parameter is provided
+  static async getAllRoutines(req, res, next) {
+    const { search } = req.query;
 
-        if(search) {
-          const data = await Routines.searchRoutine(search)
-          console.log(data);
-          return res.status(200).json({data})
-        }
-        const redisPost = await redis.get("routines")
-        if(redisPost) {
-          return res.status(200).json({data: JSON.parse(redisPost)})
-        }
-
-
-        try {
-              // Call getAllRoutines from Routines class
-              const data = await Routines.getAllRoutines();
-
-              await redis.set("routines", JSON.stringify(data))
-
-              return res.status(200).json({ data });
-            
-        } catch (error) {
-          next(error)        
-}
+    if (search) {
+      const data = await Routines.searchRoutine(search);
+      console.log(data);
+      return res.status(200).json({ data });
     }
-  };
+    const redisPost = await redis.get("routines");
+    if (redisPost) {
+      return res.status(200).json({ data: JSON.parse(redisPost) });
+    }
 
+    try {
+      // Call getAllRoutines from Routines class
+      const data = await Routines.getAllRoutines();
+
+      await redis.set("routines", JSON.stringify(data));
+
+      return res.status(200).json({ data });
+    } catch (error) {
+      next(error);
+    }
+  }
   static async createUserRoutine(req, res, next) {
     try {
       const body = req.body;
       let routineInput = {
         ...body,
-        userId: req.user._id
-      }
+        userId: req.user._id,
+      };
       const data = await Routines.createUserRoutine(routineInput);
       res.status(201).json({ data });
     } catch (error) {
       next(error);
     }
-  };
+  }
 
   static async getUserRoutines(req, res, next) {
     console.log("masuk");
     try {
       console.log(req.user, ">>>>>");
       const data = await Routines.getUserRoutines(req.user._id);
-      if (!data) throw {name: "NotFound"}
+      if (!data) throw { name: "NotFound" };
 
       res.status(200).json(data);
     } catch (error) {
       console.log(error);
       next(error);
     }
-  };
+  }
 }
 
 module.exports = RoutineController;
